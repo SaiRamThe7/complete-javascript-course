636d0c07ae3b780f55b4d5b8826d0cfa63b3af62
SAIRAM <kaligiri.sairam@dxc.com>
2025-07-23 18:40:29 +0530

    Started Developer skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,28 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const { cloneElement } = require("react");
+
+let a = 16;
+
+console.log("a");
+console.log("How are you ?");
+
+//My challenge for My Self
+
+//Need to find the max number using a function
+
+const numbers = [3, 6, 24, 65, 753, 45, 85, 75, 24, 55, 54];
+let maxNumber = Math.max(numbers); //numbers[0];
+const checkBiggestNumber = (input) => {
+  for (let i = 1; (i = input.length); i++) {
+    if (maxNumber > input[i]) {
+      maxNumber = input[i];
+    }
+  }
+  return maxNumber;
+};
+
+checkBiggestNumber(numbers);
+
+console.log(maxNumber);
