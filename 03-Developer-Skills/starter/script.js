2cbfb81999d322b28e8ccacaf8064afbc80028a4
SAIRAM <kaligiri.sairam@dxc.com>
2025-07-28 19:41:22 +0530

    Developer skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -11,64 +11,24 @@ console.log("How are you ?");
 //My challenge for My Self
 
 //Need to find the max number using a function
+var maxNumber;
+var minNumber;
 
-const numbers = [3, 6, 24, 65, 753, 45, 85, 75, 24, 55, 54];
-let maxNumber = Math.max(numbers); //numbers[0];
-// const checkBiggestNumber = (input) => {
-//   for (let i = 1; (i = input.length); i++) {
-//     if (maxNumber > input[i]) {
-//       maxNumber = input[i];
-//     }
-//   }
-//   return maxNumber;
-// };
+const checkBiggestNumber = function (input) {
+  let maxNumber = input[0];
+  let minNumber = input[0];
 
-//checkBiggestNumber(numbers);
+  for (let i = 1; i < input.length; i++) {
+    if (maxNumber < input[i]) maxNumber = input[i];
+    if (minNumber > input[i]) minNumber = input[i];
+  }
+  console.log(maxNumber);
+  console.log(minNumber);
+  return [maxNumber, minNumber];
+};
 
-console.log(maxNumber);
-function zero(input) {
-  return 0;
-}
-function one() {
-  return 1;
-}
-function two() {
-  return 2;
-}
-function three() {
-  return 3;
-}
-function four() {
-  return 4;
-}
-function five() {
-  return 5;
-}
-function six() {
-  return 6;
-}
-function seven() {
-  return 7;
-}
-function eight() {
-  return 8;
-}
-function nine() {
-  return 9;
-}
+var [maxNumber, minNumber] = checkBiggestNumber([4, 53, 64, 798, 34, 534, 56]);
 
-function plus() {
-  return Math.operator("+");
-}
-function minus() {
-  return "-";
-}
-function times() {
-  return "*";
-}
-function dividedBy() {
-  return "/";
-}
-
-console.log(5 + 2);
-console.log(four(plus(five())));
+console.log(
+  `The max number is ${maxNumber} and minimum number is ${minNumber}`
+);
