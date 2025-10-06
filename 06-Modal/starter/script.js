86f5ebb8354e36d9dd1a6173dd92842c7c517329
SAIRAM <kaligiri.sairam@dxc.com>
2025-10-06 18:59:46 +0530

    New pig game started

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -9,21 +9,21 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const unhide = function () {
   modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  overlay.classList.remove('overlay');
 };
 
 const hide = function () {
   modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  overlay.classList.add('overlay');
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', unhide());
-  btnCloseModal.addEventListener('click', hide());
+  btnsOpenModal[i].addEventListener('click', unhide); //in here hide function not needed ()
+  btnCloseModal.addEventListener('click', hide);
   //   console.log(btnsOpenModal[[i]]);
-  //   document.addEventListener('keydown', function (e) {
-  //     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-  //       hide();
-  //     }
-  //   });
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      hide();
+    }
+  });
 }
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const diceIMG = document.querySelector('.dice');
+const number = function () {
+  return Math.trunc(Math.random() * 6) + 1;
+};
+const rolldice = document.querySelector('.btn--roll');
+
+score0.textContent = 0;
+score1.textContent = 0;
+diceIMG.classList.add('hidden');
+//console.log(number);
+
+rolldice.addEventListener('click', function () {
+  let numbers = number();
+  console.log(numbers);
+  diceIMG.classList.remove('hidden');
+  diceIMG.src = `dice-${numbers}.png`;
+});
