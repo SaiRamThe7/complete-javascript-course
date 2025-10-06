a0f336819c54cc6892080567e33183235231d0af
Sairam <80045866+SaiRamThe7@users.noreply.github.com>
2025-10-06 23:51:50 +0530

    Update script.js
    pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,20 +1,38 @@
 'use strict';
-const score0 = document.getElementById('score--0');
-const score1 = document.getElementById('score--1');
+const score0 = document.getElementById('current--0');
+const score1 = document.getElementById('current--1');
+const playerScore0 = document.getElementById('score--0');
+const playerScore1 = document.getElementById('score--1');
 const diceIMG = document.querySelector('.dice');
+
 const number = function () {
   return Math.trunc(Math.random() * 6) + 1;
 };
 const rolldice = document.querySelector('.btn--roll');
 
-score0.textContent = 0;
-score1.textContent = 0;
+playerScore0.textContent = 0;
+playerScore1.textContent = 0;
 diceIMG.classList.add('hidden');
 //console.log(number);
+let scoreboard = 0
 
+//clicking on roll dice function 
 rolldice.addEventListener('click', function () {
   let numbers = number();
   console.log(numbers);
+
+  //Making the image to be shown
   diceIMG.classList.remove('hidden');
+
+  //showing the dice number image
   diceIMG.src = `dice-${numbers}.png`;
+
+  //score need to be shown for the player
+if(numbers != '0'){
+    scoreboard += numbers;
+    score0.textContent = scoreboard;
+}
+else {
+    
+}
 });
