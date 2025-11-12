b358d11e24231163cf187e6c480b539b095ab66f
SAIRAM <kaligiri.sairam@dxc.com>
2025-11-12 18:43:02 +0530

    Behind

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+const myname = 'sai';
+
+function greet() {
+  console.log(`Hello, ${myname}! Welcome to the behind-the-scenes tutorial.`);
+}
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+let x = 10;
+var y = 20;
+const z = 30;
