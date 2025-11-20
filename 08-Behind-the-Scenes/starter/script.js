2757ce6a54110f7cd5449163816b05844ba68c2b
SAIRAM <kaligiri.sairam@dxc.com>
2025-11-20 18:59:07 +0530

    behind the scenes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -11,3 +11,36 @@ marriedAmitab.lastName = 'bachan';
 
 console.log('Before marriage:', Amitab);
 console.log('After marriage:', marriedAmitab);
+
+//shallow clone vs deep clone
+
+//shallow clone
+
+const sairam = {
+  firstName: 'sairam',
+  lastName: 'kaligiri',
+  age: 26,
+  family: ['shanmugam', 'shalini'],
+};
+
+const marriedsairam = { ...sairam };
+
+marriedsairam.lastName = 'kumar';
+
+console.log('Before marriage:', sairam);
+console.log('After marriage:', marriedsairam);
+
+const sairamoriginal = {
+  firstName: 'sairam',
+  lastName: 'kaligiri',
+  age: 26,
+  family: ['shanmugam', 'shalini'],
+};
+
+const sairamcopy = structuredClone(sairamoriginal);
+
+sairamcopy.family.push('lakshmi');
+sairamcopy.family.push('vishnu');
+
+console.log('original :', sairamoriginal);
+console.log('copy :', sairamcopy);
